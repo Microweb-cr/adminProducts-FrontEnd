ef2e5e4f9864b0be7bf51f30c6083fc5efd73220
MicroWeb-cr <errold222@gmail.com>
2024-05-11 13:32:13 -0600

    Update Products.tsx

--- src/views/Products.tsx
+++ src/views/Products.tsx
@@ -47,7 +47,7 @@ export default function Products() {
                                 key={product.id}
                                 product={product}
                             />
-                        ))}
+                        )}
                     </tbody>
                 </table>
             </div>
